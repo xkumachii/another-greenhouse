7522301ef4f1e249ba8833e1e211dcd4bc93e5bd
Luis Alva <alvaluis563@gmail.com>
2017-08-07 16:14:36 -0700

    added new code

--- main.py
+++ main.py
@@ -15,10 +15,20 @@
 # limitations under the License.
 #
 import webapp2
+import os
+from google.appengine.api import users
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' %
+                users.create_login_url('/'))
+
+        self.response.write('<html><body>%s</body></html>' % greeting)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
